5ee3274f75021982702c2f14144b445a99255f31
Kulpot <kulpotwahu@gmail.com>
2023-06-28 23:20:11 +0800

    Add project files.

---
+++ DebuggerDisplayAttribute/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+//ref link:https://www.youtube.com/watch?v=QUhI2BbP8VA&list=PLRwVmtr-pp05brRDYXh-OTAIi-9kYcw3r&index=14
+// Debug > Window > Watch > Watch1
+
+[DebuggerDisplay("{Name}, Amount of meat: {Weight}")]
+class Cow
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Weight { get; set; }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        var cow = new Cow { Name = "Betsy", Age = 5, Weight = 1500 };
+        Console.WriteLine(cow);
+    }
+}
